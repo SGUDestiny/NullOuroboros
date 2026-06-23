336347e17ac55b8a15958d326fdedec697e8d0af
SGUDestiny <panormov2006@gmail.com>
2026-06-23 21:52:09 +0400

    Made vanilla music stop on the verge

---
+++ src/main/java/destiny/null_ouroboros/mixin/MusicSuppressionMixin.java
@@ -0,0 +1,28 @@
+package destiny.null_ouroboros.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.sounds.Music;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Minecraft.class)
+public class MusicSuppressionMixin {
+    @Inject(method = "getSituationalMusic", at = @At("HEAD"), cancellable = true)
+    private void suppressDarkWorldMusic(CallbackInfoReturnable<Music> cir) {
+        Minecraft mc = (Minecraft) (Object) this;
+        LocalPlayer player = mc.player;
+
+        if (player != null && isVergeOfReality(player.level())) {
+            cir.setReturnValue(new Music(SoundEvents.MUSIC_GAME, Integer.MAX_VALUE, Integer.MAX_VALUE, false));
+        }
+    }
+
+    private static boolean isVergeOfReality(Level level) {
+        return level.dimension().location().getPath().contains("verge_of_reality");
+    }
+}
